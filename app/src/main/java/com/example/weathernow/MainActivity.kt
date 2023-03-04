ca03fcb9fde5edad1f33d966eb55a19cb7bf7615
ankesh2623 <ankeshkumarchaubey@gmail.com>
2023-03-04 14:23:20 +0530

    version up to the login and sign up page and signup btn on login page linked to the signup page.

---
+++ app/src/main/java/com/example/weathernow/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.weathernow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button;
+import android.content.Intent;
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_up_page.*
+import android.view.View;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnSignUp.setOnClickListener {
+            val intent=Intent(this,SignUpPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
